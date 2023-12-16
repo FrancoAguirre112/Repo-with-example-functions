52be4cd012d6467274b4e9cc66697cd4bac6bed8
Franco Aguirre <francoaguirretrabajo@gmail.com>
2023-12-15 22:27:26 -0300

    up

--- app/page.js
+++ app/page.js
@@ -1,113 +1,192 @@
-import Image from 'next/image'
+"use client";
+import { useState } from "react";
+import * as supa from "/app/supabase.service";
+import { uuid } from "short-uuid";
 
 export default function Home() {
+  const [subscribers, setSubscribers] = useState(null);
+  const [IDs, setIDs] = useState(null);
+  const [groups, setGroups] = useState(null);
+  const [groups_subs, setGroups_subs] = useState(null);
+  const [general_subs, setGeneral_subs] = useState(null);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center gap-10 p-24">
+      {/* addToDB test */}
+      <button
+        onClick={() => {
+          supa.addToDB("SUBSCRIBERS", {
+            id: uuid(),
+            first_name: "Franco",
+            last_name: "Aguirre",
+            email: "email@email.com",
+            phone_number: "1126590757",
+          });
+
+          supa.addToDB("SUBSCRIBERS", {
+            id: uuid(),
+            first_name: "Chris",
+            last_name: "Awoke",
+            email: "email2@email.com",
+            phone_number: "1126590754",
+          });
+        }}
+      >
+        addToDB
+      </button>
+
+      {/* readAllDB test */}
+      <button
+        onClick={async () => {
+          const subs = await supa.readAllDB("SUBSCRIBERS");
+          setSubscribers(subs);
+        }}
+      >
+        readAllDB
+      </button>
+      {/* Show readAllDb results */}
+      <div className="flex flex-col gap-1">
+        <h2>Subscribers table</h2>
+        {subscribers?.map((sub) => (
+          <p key={sub.id}>
+            {sub.id} | {sub.first_name} | {sub.email}
+          </p>
+        ))}
+      </div>
+
+      {/* Delete the element from subscribers with the email "email@email.com" */}
+      <button
+        onClick={() => {
+          supa.deleteDBData("SUBSCRIBERS", "email", "email@email.com");
+        }}
+      >
+        deleteDBData
+      </button>
+
+      {/* fetchID from the subscriber with the email "email@email.com" */}
+      <button
+        onClick={async () => {
+          const fetchedID = await supa.fetchID(
+            "SUBSCRIBERS",
+            "email",
+            "email2@email.com"
+          );
+          setIDs(fetchedID);
+        }}
+      >
+        fetchID
+      </button>
+
+      {/* Show the fetchedID */}
+      <div>
+        <h2>Fetched ID</h2>
+        <p>{IDs && IDs?.map((id) => id.id)}</p>
+      </div>
+
+      {/* createGroup test */}
+      <section className="flex gap-2">
+        <button
+          onClick={() => {
+            supa.createGroup({ id: uuid(), name: "Test group" });
+          }}
+        >
+          createGroup
+        </button>
+
+        <button
+          onClick={async () => {
+            const groups = await supa.readAllDB("GROUPS");
+            setGroups(groups);
+          }}
+        >
+          readGroups
+        </button>
+      </section>
+
+      {/* Show groups */}
+      <div className="flex flex-col gap-1">
+        <h2>Groups table</h2>
+        {groups?.map((group) => (
+          <p key={group.id}>
+            {group.id} | {group.name}
+          </p>
+        ))}
+      </div>
+
+      {/* addSubscriber test, should automatically relate the created subscriber to the general group in the
+      SUBSCRIBERS_GROUPS table */}
+      <button
+        onClick={() => {
+          supa.addSubscriber({
+            id: uuid(),
+            first_name: "This subscriber",
+            last_name: "Should be on general",
+            email: "fake@email.com",
+            phone_number: "1111111111",
+          });
+        }}
+      >
+        addSubscriber
+      </button>
+
+      {/* We will now read related data, our objective is to get only the subscribers that are in the general group */}
+      <button
+        onClick={async () => {
+          // Read the SUBSCRIBERS table
+          const subs = await supa.readAllDB("SUBSCRIBERS");
+          setSubscribers(subs);
+
+          // Read the relational table GROUPS_SUBSCRIBERS
+          const groups_subs = await supa.readAllDB("GROUPS_SUBSCRIBERS");
+          setGroups_subs(groups_subs);
+
+          // Read only the subs on the general group, I want it to return a list with all the subs that fulfill the condition
+          //and I want it to return only the id, first_name and last_name of the subs
+          const general_subs = await supa.readRelatedData(
+            "GROUPS",
+            "general",
+            "SUBSCRIBERS",
+            "id, first_name, last_name"
+          );
+          setGeneral_subs(general_subs);
+          console.log(general_subs);
+        }}
+      >
+        readTables
+      </button>
+      <section className="flex flex-col gap-5">
+        {/* Show SUBSCRIBERS table */}
+        <div className="flex flex-col gap-1">
+          <h2>SUBSCRIBERS</h2>
+          {subscribers?.map((sub) => (
+            <p key={sub.id}>
+              {sub.id} | {sub.first_name} {sub.last_name}
+            </p>
+          ))}
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        {/* Show GROUPS_SUBSCRIBERS table */}
+        <div className="flex flex-col gap-1">
+          <h2>GROUPS_SUBSCRIBERS</h2>
+          <p>group_id | subscriber_id</p>
+          {groups_subs?.map((group_sub) => (
+            <p key={group_sub.group_id}>
+              {group_sub.group_id} | {group_sub.subscriber_id}
+            </p>
+          ))}
+        </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        {/* Show GROUPS_SUBSCRIBERS table */}
+        <div className="flex flex-col gap-1">
+          <h2>SUBSCRIBERS IN GENERAL GROUP</h2>
+          {general_subs &&
+            general_subs[0]?.SUBSCRIBERS?.map((gen_sub) => (
+              <p key={gen_sub.id}>
+                {gen_sub.id} | {gen_sub.first_name} {gen_sub.last_name}
+              </p>
+            ))}
+        </div>
+      </section>
     </main>
-  )
+  );
 }
---
+++ app/supabase.service.js
@@ -0,0 +1,116 @@
+import { createClient } from "@supabase/supabase-js";
+
+//TODO:Replace with the nextjs way of importing before deploy
+// export const supabase = createClient(
+//   import.meta.env.VITE_SUPA_REST_URL,
+//   import.meta.env.VITE_SUPA_ANON_KEY
+// );
+
+//TODO:Replace with env var call before deploy
+export const supabase = createClient(
+  "https://uqehjvabrmmtkziwgkuw.supabase.co/",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxZWhqdmFicm1tdGt6aXdna3V3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE3MDUzODEsImV4cCI6MjAxNzI4MTM4MX0.2UOibWpxRlxHy6hKyH9bZz3s9G2sfaKh8bUkmE9e55E"
+);
+
+//Adds data to a table given it's name
+export const addToDB = async (tableName, dbData) => {
+  const { error } = await supabase.from(tableName).insert([dbData]).select();
+
+  if (error) throw error;
+};
+
+//Simply reads all the data from a table given it's name
+export const readAllDB = async (tableName) => {
+  const { data, error } = await supabase.from(tableName).select("*");
+  if (error) {
+    throw error;
+  }
+
+  return data;
+};
+
+//Deletes a row based on the value of one of its columns
+export const deleteDBData = async (tableName, columnName, columnValue) => {
+  const { error } = await supabase
+    .from(tableName)
+    .delete()
+    .eq(columnName, columnValue);
+
+  if (error) {
+    throw error;
+  }
+
+  console.log("Deletion successful");
+};
+
+//Returns related data from one table (fromTable)
+export const readRelatedData = async (
+  fromTable,
+  fromValueName,
+  selectedTable,
+  selectedData
+) => {
+  const { data, error } = await supabase
+    .from(fromTable)
+    .select(`*, ${selectedTable} (${selectedData})`)
+    .eq("name", fromValueName);
+
+  if (error) throw error;
+  console.log(data);
+  return data;
+};
+
+//Fetches the id of one value given the tableName where it is located, the column we wan't to identify it for
+//and which value that column should have. ex: "GROUPS", "name", "general" will fetch the id of the general group
+export const fetchID = async (tableName, column, columnValue) => {
+  try {
+    const { data, error } = await supabase
+      .from(tableName)
+      .select("id")
+      .eq(column, columnValue);
+
+    if (error) {
+      console.error("Error fetching ID:", error);
+      return null;
+    }
+
+    //Since we don't know if the users are going to register more than one, there might be duplicated people in the database with
+    //different data, so this will return an array of id's just in case for example we ask for the id of a subscriber with the email "email@email.com" and there are multiple
+    return data;
+  } catch (error) {
+    console.error("Error fetching ID:", error.message);
+    return null;
+  }
+};
+
+//Creates a group given a group object { id:uuid() , name:string }
+export const createGroup = (group) => {
+  addToDB("GROUPS", group);
+};
+
+//Adds a subscriber to a group given the subscriber email and the group name
+export const addSubscriberToGroup = async (subscriberID, groupName) => {
+  try {
+    //We fetch the id from the specified group
+    const groups = await fetchID("GROUPS", "name", groupName);
+
+    //add the group_id and subscriber_id to the GROUPS_SUBSCRIBERS table to make them related
+
+    addToDB("GROUPS_SUBSCRIBERS", {
+      group_id: groups[0].id, // Assuming groups are unique
+      subscriber_id: subscriberID,
+    });
+  } catch (error) {
+    console.error("Error adding subscriber to group:", error);
+    // Handle error or throw it further if necessary
+  }
+};
+
+//Adds a subscriber to our database, also adds it to the general subscriber group by default
+export const addSubscriber = (subscriber) => {
+  //We add the subscriber to the SUBSCRIBERS table
+  addToDB("SUBSCRIBERS", subscriber);
+
+  //We add the subscriber the general group by default
+  addSubscriberToGroup(subscriber.id, "general");
+};
